880a86c476f42b1157616eddaa6d52052f1287ff
Dan Callaghan <dcallaghan@me.com>
2022-10-02 14:21:07 +1100

    chore(nvim/telescope): add some new mappings

--- nvim/.config/nvim/lua/dcal/plugin/telescope.lua
+++ nvim/.config/nvim/lua/dcal/plugin/telescope.lua
@@ -20,7 +20,9 @@ local map = vim.keymap.set
 local silent_noremap = { silent = true, noremap = true }
 
 map('n', '<leader>ff', function() require('telescope.builtin').find_files() end, silent_noremap)
+map('n', '<leader>fd', function() require('telescope.builtin').find_files({ cwd = vim.fn.expand('%:p:h') }) end, silent_noremap)
 map('n', '<leader>fb', function() require('telescope.builtin').buffers() end, silent_noremap)
 map('n', '<leader>fa', function() require('telescope.builtin').grep_string({ search = vim.fn.input('search> ')}) end, silent_noremap)
 map('n', '<leader>fs', function() require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') }) end, silent_noremap)
 map('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, silent_noremap)
+map('n', '<leader>fc', function() require('telescope.builtin').live_grep({ cwd = vim.fn.expand('%:p:h') }) end, silent_noremap)
